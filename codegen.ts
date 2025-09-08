08ea32a05cb09b2f73f3133074cccba050de30d8
jonathanprozzi <jonathanprozzi@gmail.com>
2025-09-07 20:20:55 -0400

    Updates API URL and runs codegen

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "https://testnet.c.intuition.sh/v1/graphql",
+  schema: "https://testnet.next.intuition.sh/v1/graphql",
   documents: ["graphql/**/*.ts"],
   generates: {
     "./graphql/generated/graphql.ts": {
